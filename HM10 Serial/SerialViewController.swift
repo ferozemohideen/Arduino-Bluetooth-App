53f0082a5bc29017a5214f82554a12b58ba12dd6
Feroze Mohideen <fm87@duke.edu>
2018-06-25 22:25:17 -0400

    first commit

---
+++ HM10 Serial/SerialViewController.swift
@@ -0,0 +1,283 @@
+//
+//  SerialViewController.swift
+//  HM10 Serial
+//
+//  Created by Alex on 10-08-15.
+//  Copyright (c) 2015 Balancing Rock. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+import QuartzCore
+
+/// The option to add a \n or \r or \r\n to the end of the send message
+enum MessageOption: Int {
+    case noLineEnding,
+         newline,
+         carriageReturn,
+         carriageReturnAndNewline
+}
+
+/// The option to add a \n to the end of the received message (to make it more readable)
+enum ReceivedMessageOption: Int {
+    case none,
+         newline
+}
+
+final class SerialViewController: UIViewController, UITextFieldDelegate, BluetoothSerialDelegate, LineChartDelegate {
+    
+
+//MARK: IBOutlets
+    
+    @IBOutlet weak var mainTextView: UITextView!
+    @IBOutlet weak var messageField: UITextField!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint! // used to move the textField up when the keyboard is present
+    @IBOutlet weak var barButton: UIBarButtonItem!
+    @IBOutlet weak var navItem: UINavigationItem!
+    
+    
+    var label = UILabel()
+    var lineChart: LineChart!
+
+//MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // init serial
+        serial = BluetoothSerial(delegate: self)
+        
+        // UI
+        mainTextView.text = ""
+        reloadView()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(SerialViewController.reloadView), name: NSNotification.Name(rawValue: "reloadStartViewController"), object: nil)
+        
+        // we want to be notified when the keyboard is shown (so we can move the textField up)
+        NotificationCenter.default.addObserver(self, selector: #selector(SerialViewController.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(SerialViewController.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        // to dismiss the keyboard if the user taps outside the textField while editing
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SerialViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        
+        // style the bottom UIView
+        bottomView.layer.masksToBounds = false
+        bottomView.layer.shadowOffset = CGSize(width: 0, height: -1)
+        bottomView.layer.shadowRadius = 0
+        bottomView.layer.shadowOpacity = 0.5
+        bottomView.layer.shadowColor = UIColor.black.cgColor
+        
+
+        
+
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func keyboardWillShow(_ notification: Notification) {
+        // animate the text field to stay above the keyboard
+        var info = (notification as NSNotification).userInfo!
+        let value = info[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        let keyboardFrame = value.cgRectValue
+        
+        //TODO: Not animating properly
+        UIView.animate(withDuration: 1, delay: 0, options: UIViewAnimationOptions(), animations: { () -> Void in
+            self.bottomConstraint.constant = keyboardFrame.size.height
+            }, completion: { Bool -> Void in
+            self.textViewScrollToBottom()
+        })
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        // bring the text field back down..
+        UIView.animate(withDuration: 1, delay: 0, options: UIViewAnimationOptions(), animations: { () -> Void in
+            self.bottomConstraint.constant = 0
+        }, completion: nil)
+
+    }
+    
+    func reloadView() {
+        // in case we're the visible view again
+        serial.delegate = self
+        
+        if serial.isReady {
+            navItem.title = serial.connectedPeripheral!.name
+            barButton.title = "Disconnect"
+            barButton.tintColor = UIColor.red
+            barButton.isEnabled = true
+        } else if serial.centralManager.state == .poweredOn {
+            navItem.title = "Bluetooth Serial"
+            barButton.title = "Connect"
+            barButton.tintColor = view.tintColor
+            barButton.isEnabled = true
+        } else {
+            navItem.title = "Bluetooth Serial"
+            barButton.title = "Connect"
+            barButton.tintColor = view.tintColor
+            barButton.isEnabled = false
+        }
+    }
+    
+    func textViewScrollToBottom() {
+        let range = NSMakeRange(NSString(string: mainTextView.text).length - 1, 1)
+        mainTextView.scrollRangeToVisible(range)
+    }
+    
+
+//MARK: BluetoothSerialDelegate
+    var data: [CGFloat] = [0]
+    var sSubview = UIView()
+    var count = 0
+    func serialDidReceiveString(_ message: String) {
+        sSubview = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width * 1, height: view.frame.height * 1))
+        sSubview.backgroundColor = UIColor.white // just for test, to make it visible
+        sSubview.center = view.center // that s pretty easy!
+        
+        view.addSubview(sSubview)
+//        if count != 0 {
+//            lineChart.x.labels.visible = false
+//        }
+        // add the received text to the textView, optionally with a line break at the end
+        mainTextView.text! += message
+        let pref = UserDefaults.standard.integer(forKey: ReceivedMessageOptionKey)
+        if pref == ReceivedMessageOption.newline.rawValue { mainTextView.text! += "\n" }
+        textViewScrollToBottom()
+        
+        var views: [String: AnyObject] = [:]
+        //label.text = "..."
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.center
+        self.view.addSubview(label)
+        views["label"] = label
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[label]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-80-[label]", options: [], metrics: nil, views: views))
+        
+        let str = message
+        let fl = CGFloat((str as NSString).floatValue)
+        data.append(fl)
+        
+        // simple line with custom x axis labels
+        //xLabels+=["*"]
+        
+        let xLabels: [String] = ["*", "*", "*", "*", "*"]
+        
+        lineChart = LineChart()
+        lineChart.animation.enabled = true
+        lineChart.area = false
+        lineChart.x.labels.visible = false
+        lineChart.x.grid.count = 5
+        lineChart.y.grid.count = 5
+        lineChart.x.labels.values = xLabels
+        lineChart.y.labels.visible = true
+        lineChart.addLine(data)
+        
+        lineChart.translatesAutoresizingMaskIntoConstraints = false
+        lineChart.delegate = self
+        self.view.addSubview(lineChart)
+        views["chart"] = lineChart
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[chart]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[label]-[chart(==200)]", options: [], metrics: nil, views: views))
+        
+        count = count+1
+        //lineChart.clear()
+        //lineChart.addLine(data)
+    }
+    
+    func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
+        reloadView()
+        dismissKeyboard()
+        let hud = MBProgressHUD.showAdded(to: view, animated: true)
+        hud?.mode = MBProgressHUDMode.text
+        hud?.labelText = "Disconnected"
+        hud?.hide(true, afterDelay: 1.0)
+    }
+    
+    func serialDidChangeState() {
+        reloadView()
+        if serial.centralManager.state != .poweredOn {
+            dismissKeyboard()
+            let hud = MBProgressHUD.showAdded(to: view, animated: true)
+            hud?.mode = MBProgressHUDMode.text
+            hud?.labelText = "Bluetooth turned off"
+            hud?.hide(true, afterDelay: 1.0)
+        }
+    }
+    
+    
+//MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if !serial.isReady {
+            let alert = UIAlertController(title: "Not connected", message: "What am I supposed to send this to?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: { action -> Void in self.dismiss(animated: true, completion: nil) }))
+            present(alert, animated: true, completion: nil)
+            messageField.resignFirstResponder()
+            return true
+        }
+        
+        // send the message to the bluetooth device
+        // but fist, add optionally a line break or carriage return (or both) to the message
+        let pref = UserDefaults.standard.integer(forKey: MessageOptionKey)
+        var msg = messageField.text!
+        switch pref {
+        case MessageOption.newline.rawValue:
+            msg += "\n"
+        case MessageOption.carriageReturn.rawValue:
+            msg += "\r"
+        case MessageOption.carriageReturnAndNewline.rawValue:
+            msg += "\r\n"
+        default:
+            msg += ""
+        }
+        
+        // send the message and clear the textfield
+        serial.sendMessageToDevice(msg)
+        messageField.text = ""
+        return true
+    }
+    
+    func dismissKeyboard() {
+        messageField.resignFirstResponder()
+    }
+    
+    
+//MARK: IBActions
+
+    @IBAction func barButtonPressed(_ sender: AnyObject) {
+        if serial.connectedPeripheral == nil {
+            performSegue(withIdentifier: "ShowScanner", sender: self)
+        } else {
+            serial.disconnect()
+            reloadView()
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+    /**
+     * Line chart delegate method.
+     */
+    func didSelectDataPoint(_ x: CGFloat, yValues: Array<CGFloat>) {
+        label.text = "x: \(x)     y: \(yValues)"
+    }
+    
+    
+    
+    /**
+     * Redraw chart on device rotation.
+     */
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        if let chart = lineChart {
+            chart.setNeedsDisplay()
+        }
+    }
+}
